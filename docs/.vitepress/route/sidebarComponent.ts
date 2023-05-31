2d348d908a9bb1e3da6bb341c61d1816eec2dc31
zyi <1182900548@qq.com>
2023-05-31 10:35:50 +0800

    删除多余注释

--- docs/.vitepress/route/sidebarComponent.ts
+++ docs/.vitepress/route/sidebarComponent.ts
@@ -9,11 +9,8 @@ fs.readdirSync('./docs/component').map(pkg => {
       text: pkg.split('-')[1],
       collapsed: false,
       items: fs.readdirSync(`./docs/component/${pkg}`).map(file => {
-        // 将文件名转为首字母大写
         const text = file.replace('.md', '').split('-')[1]
-        // 将文件名转为首字母大写
         const link = `/component/${pkg}/${file.replace('.md', '')}`
-        // 将文件名转为首字母大写
         return { text, link }
       })
     })
